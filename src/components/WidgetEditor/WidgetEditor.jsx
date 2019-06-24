41ae31e5ae2bdc08a560bb73d0a6798952a2cb1b
Aleh_khantsevich <aleh_khantsevich@epam.com>
2019-06-24 12:09:38 +0300

    fix hook

--- src/components/WidgetEditor/WidgetEditor.jsx
+++ src/components/WidgetEditor/WidgetEditor.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState, useImperativeHandle } from "react";
+import React, { useEffect, useRef, useImperativeHandle } from "react";
 import EditorJS from "@editorjs/editorjs";
 import { formatExport, importFromJson } from "../helpers/jsonHelper";
 import { createConfig } from "../helpers/configCreator";
 
 const WidgetEditor = ({ data, types }, ref) => {
-  const editor = useState();
+  const editor = useRef();
 
   useImperativeHandle(ref, () => ({
     async save() {
